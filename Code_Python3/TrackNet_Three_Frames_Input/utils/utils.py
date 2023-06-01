322a179895c281ee77f145d36606f7019329d78c
MooreManor <liziqing9908@126.com>
2023-06-01 19:49:41 +0800

    add: speed && loc csv gen

--- Code_Python3/TrackNet_Three_Frames_Input/utils/utils.py
+++ Code_Python3/TrackNet_Three_Frames_Input/utils/utils.py
@@ -2,6 +2,7 @@ import os
 import os.path as osp
 import subprocess
 import cv2
+import numpy as np
 
 def video_to_images(vid_file, img_folder=None, return_info=False):
     if img_folder is None:
@@ -31,7 +32,7 @@ def save_PIL_image(PIL_img, img_name, img_folder):
     os.makedirs(img_folder, exist_ok=True)
     PIL_img.save(osp.join(img_folder, img_name))
 
-def gen_tennis_loc_csv(folder, data=None, file_name='tennis_loc.csv'):
+def gen_tennis_loc_csv(folder, data=None, file_name='tennis.csv'):
     import csv
     # 定义 CSV 文件路径和字段名
     csv_file_path = osp.join(folder, file_name)
@@ -47,7 +48,114 @@ def gen_tennis_loc_csv(folder, data=None, file_name='tennis_loc.csv'):
             frame = i
             x = data[i][0]
             y = data[i][1]
+            if x is None:
+                x = 'None'
+            if y is None:
+                y = 'None'
 
             writer.writerow([frame, x, y])
 
+def read_tennis_loc_csv(folder, file_name='tennis.csv'):
+    import csv
+    # 定义 CSV 文件路径和字段名
+    csv_file_path = osp.join(folder, file_name)
+    with open(csv_file_path, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)  # 跳过第一行
+        data = []
+        for row in reader:
+            x, y = float(row[1]), float(row[2])
+            data.append([x, y])
+
+    # 将数据转换为numpy数组
+    data = np.array(data)
+    return data
+
+def gen_court_inf(folder, data):
+    # 打开文件并写入多个线的坐标
+    txt_file_path = osp.join(folder, 'lines.txt')
+    with open(txt_file_path, 'w') as f:
+        for i in range(0, len(data), 4):
+            coords = f'{data[i]},{data[i + 1]},{data[i + 2]},{data[i + 3]}'
+            f.write(coords + '\n')
+def read_court_inf(folder):
+    # 读取 gen_court_inf 函数生成的 txt 文件，返回线的坐标列表
+    txt_file_path = osp.join(folder, 'lines.txt')
+    lines = []
+    with open(txt_file_path, 'r') as f:
+        for line in f.readlines():
+            line = line.strip()
+            if line:
+                coords = line.split(',')
+                lines.append([float(coord) for coord in coords])
+    return lines
+
+
+def add_csv_col(folder, new_col_name, data=None, file_name='tennis_loc.csv'):
+    import csv
+    csv_file_path = osp.join(folder, file_name)
+
+    # 读取csv文件
+    with open(csv_file_path, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        rows = list(reader)
+
+    # 添加第四列数据
+    rows[0].append(new_col_name)
+    for i in range(1, len(rows)):
+        # 在这里填写添加第四列数据的代码
+        # 假设新的数据保存在变量new_data中
+        new_data = data[i-1]
+        if new_data is None:
+            new_data = 'None'
+        rows[i].append(new_data)
+
+    # 将更新后的数据写回到csv文件
+    with open(csv_file_path, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(rows)
+
+def calculate_velocity(positions):
+    velocities = []
+    # accelerations = []
+    velocities.append((None, None))
+    # accelerations.append(-1, -1)
+    # accelerations.append(-1, -1)
+    for i in range(1, len(positions)):
+        # 计算相邻两帧之间的距离和时间间隔
+        if positions[i][0]==None:
+            velocities.append((None, None))
+        else:
+            flag = 0
+            for j in range(i - 1, -1, -1):
+                if positions[j][0] != None:
+                    dx = positions[i][0] - positions[j][0]
+                    dy = positions[i][1] - positions[j][1]
+                    flag=1
+                    break
+            if flag==1:
+                dt = i-j
+                # 计算相邻两帧之间的2D速度
+                v = (dx / dt, dy / dt)
+                velocities.append(v)
+            else:
+                velocities.append((None, None))
+
+        # dx = positions[i][0] - positions[i - 1][0]
+        # dy = positions[i][1] - positions[i - 1][1]
+        # dt = 1  # 假设每帧的时间间隔为1
+        #
+        # # 计算相邻两帧之间的2D速度
+        # v = (dx / dt, dy / dt)
+        # velocities.append(v)
+
+        # if i > 1:
+        #     # 计算相邻两帧之间的2D加速度
+        #     dvx = v[0] - velocities[i - 2][0]
+        #     dvy = v[1] - velocities[i - 2][1]
+        #     a = (dvx / dt, dvy / dt)
+        #     accelerations.append(a)
+
+    # return velocities, accelerations
+    return np.array(velocities)
 
--- Code_Python3/unit_test.py
+++ Code_Python3/unit_test.py
@@ -37,38 +37,95 @@
 
 
 # loss
-#--------------------
-import tensorflow as tf
+# #--------------------
+# import tensorflow as tf
+# import numpy as np
+# # y_true = [[[0., 0.9, 1.0, 0.0, 1.0, 0.0]]]
+# # y_pred = [[[0.4, 0.6, 0.8, 0.2, 0.9, 0.2]]]
+#
+# y_true = [[[0.,0.9]]]
+# y_pred = [[[0.4,0.6]]]
+# loss = tf.keras.losses.categorical_crossentropy(np.array(y_true), np.array(y_pred))
+# loss.numpy()
+# print('tf.keras.losses.categorical_crossentropy', loss)
+#
+# loss = -(0 * tf.math.log(0.4) + 0.9 * tf.math.log(0.6))
+# print('tf.math.log', loss)
+#
+# import torch
+#
+# loss = -(0 * torch.log(torch.tensor(0.4)) + 0.9 * torch.log(torch.tensor(0.6)))
+# print('torch.loss', loss)
+#
+#
+# def pt_categorical_crossentropy(pred, label):
+#     """
+#     使用pytorch 来实现 categorical_crossentropy
+#     """
+#     # print(-label * torch.log(pred))
+#     return torch.sum(-label * torch.log(pred))
+#
+#
+# loss = pt_categorical_crossentropy(torch.tensor(y_pred), torch.tensor(y_true))
+# print(loss)
+
+
+# test
+#------------------------------
+# from TrackNet_Three_Frames_Input.utils.utils import calculate_velocity, add_csv_col
+# import csv
+# import numpy as np
+#
+# # 读取csv文件
+# with open('tennis_loc.csv', newline='') as csvfile:
+#     reader = csv.reader(csvfile)
+#     next(reader) # 跳过第一行
+#     data = []
+#     for row in reader:
+#         if row[1]=='None':
+#             x, y = None, None
+#         else:
+#             x, y = float(row[1]), float(row[2])
+#         data.append([x, y])
+#
+# # 将数据转换为numpy数组
+# data = np.array(data)
+#
+# vols = calculate_velocity(data)
+# add_csv_col(folder='./', new_col_name='x速度', data=vols[:, 0], file_name='tennis_loc.csv')
+# add_csv_col(folder='./', new_col_name='y速度', data=vols[:, 1], file_name='tennis_loc.csv')
+
 import numpy as np
-# y_true = [[[0., 0.9, 1.0, 0.0, 1.0, 0.0]]]
-# y_pred = [[[0.4, 0.6, 0.8, 0.2, 0.9, 0.2]]]
+from scipy.linalg import inv
 
-y_true = [[[0.,0.9]]]
-y_pred = [[[0.4,0.6]]]
-loss = tf.keras.losses.categorical_crossentropy(np.array(y_true), np.array(y_pred))
-loss.numpy()
-print('tf.keras.losses.categorical_crossentropy', loss)
+# 系统动态模型（状态转移矩阵）
+A = np.array([[1, 1], [0, 1]])
 
-loss = -(0 * tf.math.log(0.4) + 0.9 * tf.math.log(0.6))
-print('tf.math.log', loss)
+# 测量模型（测量矩阵）
+H = np.array([[1, 0]])
 
-import torch
+# 状态协方差矩阵（初始状态不确定性）
+P = np.array([[1000, 0], [0, 1000]])
 
-loss = -(0 * torch.log(torch.tensor(0.4)) + 0.9 * torch.log(torch.tensor(0.6)))
-print('torch.loss', loss)
+# 过程噪声协方差矩阵
+Q = np.array([[1, 0], [0, 1]])
 
+# 测量噪声协方差矩阵
+R =np.array([[1]])
 
-def pt_categorical_crossentropy(pred, label):
-    """
-    使用pytorch 来实现 categorical_crossentropy
-    """
-    # print(-label * torch.log(pred))
-    return torch.sum(-label * torch.log(pred))
+# 初始状态
+x = np.array([[0], [0]])
 
+# 测量数据
+measurements = [1, 2, 3, 4, 5]
 
-loss = pt_categorical_crossentropy(torch.tensor(y_pred), torch.tensor(y_true))
-print(loss)
+for z in measurements:
+    # 执行卡尔曼滤波
+    x_pred = np.dot(A, x)     # 预测状态
+    P_pred = np.dot(np.dot(A, P), A.T) + Q   # 预测协方差矩阵
+    K = np.dot(np.dot(P_pred, H.T), inv(np.dot(np.dot(H, P_pred), H.T) + R))   # 卡尔曼增益
+    x = x_pred + np.dot(K, (z - np.dot(H, x_pred)))    # 更新状态
+    P = np.dot((np.eye(2) - np.dot(K, H)), P_pred)     # 更新协方差矩阵
 
-
-
-
+    # 输出估计值
+    print("估计值:", x.flatten())
